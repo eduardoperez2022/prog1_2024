2cd9c773f55b328a1745ee7c2021dfaa5d7b59e7
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2024-09-10 21:45:13 -0300

    Ejercicio lectura y proceso de archivo CSV

---
+++ ejercicio_303/Ejercicio_303.java
@@ -0,0 +1,12 @@
+package ejercicio_303;
+
+public class Ejercicio_303 {
+
+    public static void main(String[] args) {
+        
+        proceso pr = new proceso();
+        pr.lectura();
+        
+    }
+    
+}
